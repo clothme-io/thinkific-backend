64becf798940aeee556dfd6d8603986bcae2e0c3
paul <paul.ikhane@outlook.com>
2022-02-19 18:34:38 -0500

    updated webhook

--- src/http/tenant/routes/webhook.ts
+++ src/http/tenant/routes/webhook.ts
@@ -7,23 +7,15 @@ var crypto = require("crypto");
 const router = express.Router();
 
 // Add A Brand: Can only be done by Employee with the right role and duties.
-router.post("/add", async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    return res.send({});
-  } catch (error) {
-    return res.send({ error: error });
+router.post(
+  "/webhooks",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      return res.send({});
+    } catch (error) {
+      return res.send({ error: error });
+    }
   }
-});
-
-// Update: To Attach Locations To Brand
-router.post(
-  "/recieved/web/hook",
-  async (req: Request, res: Response, next: NextFunction) => {}
 );
 
-// Read All Brand Per Location
-router.post(
-  "/get-all/location/brand",
-  async (req: Request, res: Response, next: NextFunction) => {}
-);
 export { router };
