67fa4f62b8a37b21df65a6f108cff30ea93db3a4
paul <paul.ikhane@outlook.com>
2022-02-19 18:43:12 -0500

    updated health check

--- src/http/tenant/routes/healthCheck.ts
+++ src/http/tenant/routes/healthCheck.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
 
 // Add A Brand: Can only be done by Employee with the right role and duties.
-router.post("", async (req: Request, res: Response, next: NextFunction) => {
+router.get("", async (req: Request, res: Response, next: NextFunction) => {
   return "All Good";
 });
 
